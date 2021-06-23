60094a0781f187066cc1c73b0b3af5a36822e235
caioluciofc <caioluciofc@gmail.com>
2021-06-23 12:34:36 -0300

    Feat: Created External Game Map, Camera Logic, Wall Collision and Solved Camera Border Bug

--- main.py
+++ main.py
@@ -1,62 +1,126 @@
 import emoji
 import os
 import colors
+import click
+import math
 
-game_map = [['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
-             '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-            ['#', '.', '.', '.', ' ', '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-             ' ', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
-             ' ', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
-             ' ', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-            ['#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.',
-             ' ', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#'],
-            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
-             '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#'],
-            ['#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#',
-             '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#', '#']]
+SCREENWIDTH = 100
+SCREENHEIGHT = 20
+CAMERAWIDTH = 40
+SPACEBETWEENLINESCAMERA = int(SCREENWIDTH/2 - (CAMERAWIDTH)) - 1
+GAMENAME = "UNICODE MAN IN THE ASCIIVERSE!"
+UNICODEMAN = '\033[93m☺\033[0m'
+user_level = 1
+user_str = 1
+user_dex = 1
+user_int = 1
+user_health = 100
+user_status = "Good"
+
+walls = ["#", "@", "|", "/", "-", "8", "\\", "_"]
+
+game_map = []
+with open('map.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        this_line = []
+        for char in line:
+            this_line.append(char)
+        game_map.append(this_line)
 
 
-def gameScreen(gamemapline):
-    print("/", end="")
-    for i in range(0, 100):
-        print('-', end="")
-    print("\\", end="")
-    print("\n", end="")
+def camera(gamemap, y, x):
     print("|", end="")
-    for i in range(0, 100):
-        print(' ', end="")
+    for k in range(0, SPACEBETWEENLINESCAMERA):
+        print(" ", end="")
+    for i in range(x-CAMERAWIDTH - 1, x+CAMERAWIDTH + 1):
+        print("@", end="")
+    for l in range(0, SPACEBETWEENLINESCAMERA):
+        print(" ", end="")
     print("|")
-    for line in gamemapline:
+    if y - int((SCREENHEIGHT / 2)) <= 0:
+        range_y_1 = 0
+        range_y_2 = SCREENHEIGHT
+    elif y + int((SCREENHEIGHT / 2)) >= len(game_map):
+        range_y_1 = len(game_map) - SCREENHEIGHT
+        range_y_2 = len(game_map)
+    else:
+        range_y_1 = y - int((SCREENHEIGHT / 2))
+        range_y_2 = y + int((SCREENHEIGHT / 2))
+    if x-CAMERAWIDTH <= 0:
+        range_x_1 = 0
+        range_x_2 = CAMERAWIDTH * 2
+    elif x+CAMERAWIDTH >= len(game_map[0]):
+        range_x_1 = (len(game_map[0]) - CAMERAWIDTH * 2) - 1
+        range_x_2 = len(game_map[0]) - 1
+    else:
+        range_x_1 = x - CAMERAWIDTH
+        range_x_2 = x + CAMERAWIDTH
+    for i in range(range_y_1, range_y_2):
         print("|", end="")
-        for i in range(0, 30):
+        for k in range(0, SPACEBETWEENLINESCAMERA):
             print(" ", end="")
-        for tile in line:
-            print(tile, end="")
-        for i in range(0, 40):
+        print("@", end="")
+        for j in range(range_x_1, range_x_2):
+            print(game_map[i][j], end="")
+        print("@", end="")
+        for l in range(0, SPACEBETWEENLINESCAMERA):
             print(" ", end="")
         print("|")
     print("|", end="")
-    for i in range(0, 100):
+    for k in range(0, SPACEBETWEENLINESCAMERA):
+        print(" ", end="")
+    for i in range(x-CAMERAWIDTH - 1, x+CAMERAWIDTH + 1):
+        print("@", end="")
+    for l in range(0, SPACEBETWEENLINESCAMERA):
+        print(" ", end="")
+    print("|")
+
+
+def gameScreen(gamemapline, y, x):
+    player_status = f"Health {user_health} Status: {user_status}"
+    player_stats = f"Level {user_level} STR {user_str} DEX {user_dex} INT {user_int}"
+    print("/", end="")
+    for i in range(0, SCREENWIDTH):
+        print('-', end="")
+    print("\\")
+    game_name_len = len(GAMENAME)
+    print("|", end="")
+    for i in range(0, int((SCREENWIDTH/2) - (game_name_len/2))):
+        print(' ', end="")
+    print(GAMENAME, end="")
+    for i in range(0, int((SCREENWIDTH/2) - (game_name_len/2))):
+        print(' ', end="")
+    print("|")
+    camera(gamemapline, y, x)
+    print("|", end="")
+    for i in range(0, SCREENWIDTH):
         print(' ', end="")
     print("|")
     print("|", end="")
-    for i in range(0, 100):
+    for i in range(0, 10):
+        print(' ', end="")
+    print(player_stats, end="")
+    for i in range(0, SCREENWIDTH - 10 - len(player_stats)):
         print(' ', end="")
     print("|")
     print("|", end="")
-    for i in range(0, 100):
+    for i in range(0, 10):
+        print(' ', end="")
+    print(player_status, end="")
+    for i in range(0, SCREENWIDTH - 10 - len(player_status)):
+        print(' ', end="")
+    print("|")
+    print("|", end="")
+    for i in range(0, SCREENWIDTH):
         print(' ', end="")
     print("|")
     print("\\", end="")
-    for i in range(0, 100):
+    for i in range(0, SCREENWIDTH):
         print('-', end="")
     print("/", end="")
     print("\n", end="")
 
-
-unicode_man = 'C'
 
 game_on = False
 
@@ -65,24 +129,43 @@ start_game = input("DO YOU WANT TU PLAY?")
 if start_game == "Yes" or start_game == "Y":
     game_on = True
 
-unicode_man_y, unicode_man_x = 3, 14
+unicode_man_y, unicode_man_x = 20, 50
 
 while game_on:
     os.system('cls')
-    old_tile = game_map[unicode_man_y][unicode_man_x]
-    game_map[unicode_man_y][unicode_man_x] = unicode_man
-    gameScreen(game_map)
+    if game_map[unicode_man_y][unicode_man_x] == UNICODEMAN:
+        pass
+    else:
+        old_tile = game_map[unicode_man_y][unicode_man_x]
+        game_map[unicode_man_y][unicode_man_x] = UNICODEMAN
+    gameScreen(game_map, unicode_man_y, unicode_man_x)
 
-    what_do_you_do = input("ML, MR, MU, MD")
-    if what_do_you_do == "ML":
-        game_map[unicode_man_y][unicode_man_x] = old_tile
-        unicode_man_y, unicode_man_x = unicode_man_y, unicode_man_x - 1
-    elif what_do_you_do == "MR":
-        game_map[unicode_man_y][unicode_man_x] = old_tile
-        unicode_man_y, unicode_man_x = unicode_man_y, unicode_man_x + 1
-    elif what_do_you_do == "MU":
-        game_map[unicode_man_y][unicode_man_x] = old_tile
-        unicode_man_y, unicode_man_x = unicode_man_y - 1, unicode_man_x
-    elif what_do_you_do == "MD":
-        game_map[unicode_man_y][unicode_man_x] = old_tile
-        unicode_man_y, unicode_man_x = unicode_man_y + 1, unicode_man_x
+    what_do_you_do = click.getchar()
+    if what_do_you_do == "a":
+        if game_map[unicode_man_y][unicode_man_x - 1] in walls:
+            continue
+        else:
+            game_map[unicode_man_y][unicode_man_x] = old_tile
+            unicode_man_y, unicode_man_x = unicode_man_y, unicode_man_x - 1
+    elif what_do_you_do == "d":
+        if game_map[unicode_man_y][unicode_man_x + 1] in walls:
+            continue
+        else:
+            game_map[unicode_man_y][unicode_man_x] = old_tile
+            unicode_man_y, unicode_man_x = unicode_man_y, unicode_man_x + 1
+    elif what_do_you_do == "w":
+        if game_map[unicode_man_y - 1][unicode_man_x] in walls:
+            continue
+        else:
+            game_map[unicode_man_y][unicode_man_x] = old_tile
+            unicode_man_y, unicode_man_x = unicode_man_y - 1, unicode_man_x
+    elif what_do_you_do == "s":
+        if game_map[unicode_man_y + 1][unicode_man_x] in walls:
+            continue
+        else:
+            game_map[unicode_man_y][unicode_man_x] = old_tile
+            unicode_man_y, unicode_man_x = unicode_man_y + 1, unicode_man_x
+    else:
+        old_tile = old_tile
+        game_map[unicode_man_y][unicode_man_x] = UNICODEMAN
+        continue
